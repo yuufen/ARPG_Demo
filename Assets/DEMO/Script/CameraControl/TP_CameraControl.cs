04794b25cba5d10e5e8a9ef0d29687ae39134647
YuuFen <603236742@qq.com>
2024-01-12 00:09:18 +0800

    feat: CameraControl

---
+++ Assets/DEMO/Script/CameraControl/TP_CameraControl.cs
@@ -0,0 +1,53 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TP_CameraControl : MonoBehaviour {
+    private GameInputManager _gameInputManager;
+
+    [Header("相机参数配置")]
+    [SerializeField] private Vector2 _cameraVerticalMaxAngle; // 相机垂直角度限制
+
+    [SerializeField] private float _controlSpeed; // 相机旋转速度
+    [SerializeField] private float _smoothSpeed; // 相机旋转 SmoothDamp 的 smoothTime
+
+    private Transform _lookTarget; // 相机 Target
+    [SerializeField] private float _positionOffset; // 相机距离
+    [SerializeField] private float _positionSmoothTime; // 相机距离
+
+    private Vector2 _input;
+    private Vector3 _cameraRotation;
+    private Vector3 _smoothDampVelocity;
+
+    private void Awake() {
+        _gameInputManager = GetComponent<GameInputManager>();
+        _lookTarget = GameObject.FindWithTag("CameraTarget").transform;
+    }
+
+    private void Update() {
+        CameraInput();
+    }
+
+    private void LateUpdate() {
+        UpdateCameraRotation();
+        CameraPosition();
+    }
+
+    private void CameraInput() {
+        _input.y += _gameInputManager.CameraLook.x * _controlSpeed; // 水平
+        _input.x -= _gameInputManager.CameraLook.y * _controlSpeed; // 垂直
+        _input.x = Mathf.Clamp(_input.x, _cameraVerticalMaxAngle.x, _cameraVerticalMaxAngle.y);
+    }
+
+    private void UpdateCameraRotation() {
+        _cameraRotation = Vector3.SmoothDamp(_cameraRotation, new Vector3(_input.x, _input.y, 0f),
+            ref _smoothDampVelocity, _smoothSpeed);
+        transform.eulerAngles = _cameraRotation;
+    }
+
+    private void CameraPosition() {
+        var newPosition = _lookTarget.position + -transform.forward * _positionOffset;
+        transform.position = Vector3.Lerp(transform.position, newPosition, Time.deltaTime * _positionSmoothTime);
+    }
+}
