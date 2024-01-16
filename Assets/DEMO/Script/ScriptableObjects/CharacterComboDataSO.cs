f8b9f3914c670fa2accaa582e0186ae756b507b7
YuuFen <603236742@qq.com>
2024-01-17 02:47:35 +0800

    feat: 添加连招和招式的 SO Class，以及 SO 占位

---
+++ Assets/DEMO/Script/ScriptableObjects/CharacterComboDataSO.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+namespace DEMO.Combo {
+    // 连招的单个招式
+    [CreateAssetMenu(fileName = "ComboData", menuName = "Create/Character/ComboData", order = 0)]
+    public class CharacterComboDataSO : ScriptableObject {
+        [SerializeField] private string _comboName; // 招式名称
+        [SerializeField] private string[] _comboHitName; // 对应的受击动画，因为一个 Clip 可能有多段攻击，所以这里是数组
+        [SerializeField] private string[] _comboParryName; // 对应的格挡动画
+        [SerializeField] private float _damage; // 伤害
+        [SerializeField] private float _coldTime; // 衔接下一段攻击的间隔时间
+        [SerializeField] private float _comboPositionOffset; // 这段招式角色与目标的距离，用来把主角吸附到敌人身边正确距离
+
+        // Export
+        public string ComboName => _comboName;
+        public string[] ComboHitName => _comboHitName;
+        public string[] ComboParryName => _comboParryName;
+        public float ComboDamage => _damage;
+        public float ComboColdTime => _coldTime;
+        public float ComboPositionOffset => _comboPositionOffset;
+
+        public int GetHitOrParryNameMaxCount() => _comboHitName.Length;
+    }
+}
---
+++ Assets/DEMO/Script/ScriptableObjects/CharacterComboSO.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace DEMO.Combo {
+    // 连招表
+    [CreateAssetMenu(fileName = "Combo", menuName = "Create/Character/Combo", order = 0)]
+    public class CharacterComboSO : ScriptableObject {
+        // 连招招式列表
+        [SerializeField] private List<CharacterComboDataSO> _allComboData = new List<CharacterComboDataSO>();
+
+        // 封装一下
+
+        // 获取连招数量
+        public int TryGetComboMaxCount() => _allComboData.Count;
+
+        // 获取招式段数
+        public int TryGetHitOrParryMaxCount(int comboIndex) => _allComboData[comboIndex].GetHitOrParryNameMaxCount();
+
+
+        // 获取招式
+        public string TryGetOneComboAction(int comboIndex) {
+            if (_allComboData.Count == 0) return null;
+
+            return _allComboData[comboIndex].ComboName;
+        }
+
+        // 获取招式的属性
+        public string TryGetHitName(int comboIndex, int hitIndex) {
+            if (_allComboData.Count == 0) return null;
+            if (_allComboData[comboIndex].GetHitOrParryNameMaxCount() == 0) return null;
+
+            return _allComboData[comboIndex].ComboHitName[hitIndex];
+        }
+
+        public string TryGetParryName(int comboIndex, int hitIndex) {
+            if (_allComboData.Count == 0) return null;
+            if (_allComboData[comboIndex].GetHitOrParryNameMaxCount() == 0) return null;
+
+            return _allComboData[comboIndex].ComboParryName[hitIndex];
+        }
+
+        public float TryGetComboDamage(int comboIndex) {
+            if (_allComboData.Count == 0) return 0f;
+
+            return _allComboData[comboIndex].ComboDamage;
+        }
+
+        public float TryGetComboColdTime(int comboIndex) {
+            if (_allComboData.Count == 0) return 0f;
+
+            return _allComboData[comboIndex].ComboColdTime;
+        }
+
+        public float TryGetComboPositionOffset(int comboIndex) {
+            if (_allComboData.Count == 0) return 0f;
+
+            return _allComboData[comboIndex].ComboPositionOffset;
+        }
+    }
+}
